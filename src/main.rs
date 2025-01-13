cf5979e0f9a35078d8cccaad5d29daa27ec49683
milovanovmaksim <milovanov160386@gmail.com>
2025-01-14 00:18:46 +0300

    basic

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use fltk::{app, button, enums::Color, frame, input, prelude::*, window};
+use fltk_flex::Flex;
+use fltk_theme::{widget_themes, ThemeType, WidgetScheme, WidgetTheme};
+
+fn main() {
+    let app = app::App::default();
+    let theme = WidgetTheme::new(ThemeType::Aero);
+    theme.apply();
+
+    let mut win = window::Window::default().with_size(400, 300);
+    let flex = Flex::default()
+        .with_size(200, 100)
+        .center_of_parent()
+        .column();
+
+    let mut inp = input::Input::default();
+    let mut frame = frame::Frame::default();
+
+    let mut btn = button::Button::new(160, 200, 80, 30, "Click");
+
+    flex.end();
+    win.end();
+    win.show();
+
+    btn.set_callback(move |b| {
+        frame.set_label(&inp.value());
+    });
+
+    app.run().unwrap();
+}
+
+// fn theme_botton(btn: &mut button::Button) {
+//     btn.clear_visible_focus();
+//     btn.set_color(Color::from_rgb(255, 0, 0).inactive());
+//     btn.set_selection_color(Color::from_rgb(255, 0, 0).darker());
+// }
